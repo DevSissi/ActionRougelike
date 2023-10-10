9f4dcbbc47ed03c3810548b25d1cec5fe694ba7d
DevSissi <sunnysissi@outlook.com>
2023-10-10 13:36:15 +0800

    Revert "Add Explosive Barrel&Update Chinese Comments"
    This reverts commit 5ad26581a870897c6b4e478c8a125be6b8950595.

--- Source/ActionRougelike/Public/SCharacter.h
+++ Source/ActionRougelike/Public/SCharacter.h
@@ -20,25 +20,19 @@ public:
 
 protected:
 
-	// 弹簧臂组件
 	UPROPERTY(VisibleAnywhere)
 	USpringArmComponent* SpringArmComp;
 
-	// 摄像机组件
 	UPROPERTY(VisibleAnywhere)
 	UCameraComponent* CameraComp;
 	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	// 角色前后移动
 	void MoveForward(float	Value);
-	// 角色左右移动
 	void MoveRight(float Value);
-	// 角色主要攻击
 	void PrimaryAttack();
 
-	// 生成的弹体类
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<AActor> ProjectileClass;
 
